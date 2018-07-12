d7cf4368123a9f42df1ee8b68ba6651069f7117b
Dave Yates <dave@coinschedule.com>
2018-07-12 13:30:53 +0100

    Sidebar toggle implemented

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log('here');
+$('#sidebar-toggle').toggle(
+    function() {
+        console.log('toggle');
+        $('.sidebar').css('width', '0')
+        $('.content').css('width', '100%')
+    }, function() {
+        $('.sidebar').css('width', '30%')
+        $('.content').css('width', '70%')
+})
