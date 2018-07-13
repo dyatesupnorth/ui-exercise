17e2dab537a7da3dbe50bd7c41a4e7156360aa33
Dave Yates <dave@coinschedule.com>
2018-07-13 16:54:52 +0100

    Sidebar hiding properly.

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 $('#sidebar-toggle').toggle(
     function () {
-        $('.sidebar').animate({ 'width': '0px' }, 'slow');
+        $('.sidebar').animate({ 'width': '0px' }, 'slow',function(){
+            $('.sidebar').hide();
+        });
         $('.content').animate({ 'width': '100%' }, 'slow');
+       
+
     },
     function () {
+        $('.sidebar').show();
+
         $('.sidebar').animate({ 'width': '30%' }, 'slow');
         $('.content').animate({ 'width': '70%' }, 'slow');
     }
